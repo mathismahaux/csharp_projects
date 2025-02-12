bb6dd7c05c4f24facf706674079d58130174fff1
Mathis <mathis.mahaux@gmail.com>
2025-02-12 13:57:02 +0100

    adds exercises

---
+++ exercises/orderIdGenerator/Program.cs
@@ -0,0 +1,26 @@
+﻿// Faire cela au lieu d'écrire un commentair à chaque ligne de code.
+/*
+    Le code suivant génère 5 identifiants de commande aléatoires. 
+    Chaque identifiant de commande est composé d'un préfixe et d'un suffixe. 
+    Le préfixe est une lettre majuscule aléatoire comprise entre A et E. 
+    Le suffixe est un nombre aléatoire compris entre 1 et 999, complété par des zéros à gauche pour obtenir une 
+    longueur de 3 caractères. 
+    Exemple: A123.
+*/
+
+Random random = new Random();
+string[] orderIDs = new string[5];
+
+for (int i = 0; i < orderIDs.Length; i++)
+{
+    int prefixValue = random.Next(65, 70);
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    string suffix = random.Next(1, 1000).ToString("000");
+
+    orderIDs[i] = prefix + suffix;
+}
+
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
+}
---
+++ exercises/switch/Program.cs
@@ -0,0 +1,49 @@
+﻿int employeeLevel = 200;
+string employeeName = "John Smith";
+
+string title = "";
+
+switch (employeeLevel)
+{
+    case 100:
+        title = "Junior Associate";
+        break;
+    case 200:
+        title = "Senior Associate";
+        break;
+    case 300:
+        title = "Manager";
+        break;
+    case 400:
+        title = "Senior Manager";
+        break;
+    default:
+        title = "Associate";
+        break;
+}
+
+Console.WriteLine($"{employeeName}, {title}");
+
+int employeeLevel2 = 100;
+string employeeName2 = "John Smith";
+
+string title2 = "";
+
+switch (employeeLevel2)
+{
+    case 100:
+    case 200:
+        title2 = "Senior Associate";
+        break;
+    case 300:
+        title2 = "Manager";
+        break;
+    case 400:
+        title2 = "Senior Manager";
+        break;
+    default:
+        title2 = "Associate";
+        break;
+}
+
+Console.WriteLine($"{employeeName2}, {title2}");
