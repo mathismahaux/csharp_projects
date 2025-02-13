0576c0eed0588a1895a50539b6672343ba4c8a2b
Mathis <mathis.mahaux@gmail.com>
2025-02-13 18:11:41 +0100

    Questions 1 to 10

---
+++ exam/Program.cs
@@ -0,0 +1,117 @@
+﻿// 1. Given the following code snippet, what will the method MultiplyNumbers() return when called with the arguments 3 and 4?
+
+// public int MultiplyNumbers(int a, int b)
+// {
+//     return a * b;
+// }
+
+// "34"
+// 10
+// 7
+// 1
+// 12
+
+// Answer: 12
+
+
+// 2. Which of the following options represents the correct way to declare and assign a string variable?
+
+// string MyString = "Hello";
+// string MyString;
+// string = "Hello";
+// MyString = "Hello";
+// new string MyString = "Hello";
+
+// Answer: string MyString = "Hello";
+
+
+// 3. What is the difference between a void method and a method with a return type?
+
+// A void method can only be used in class libraries.
+// A void method always returns a value.
+// A void method does not return a value, while a method with a return type returns a specific value.
+// A void method  cannot be called from another method.
+// A method with a return type can only return numeric values.
+
+// Answer: A void method does not return a value, while a method with a return type returns a specific value.
+
+
+// 4. How do you convert an integer variable named age to a string?
+
+// string.Parse(age);
+// age.ToString("D");
+// Convert.ToUInt32(age);
+// Convert.ToString(age);
+// AesGcm.ToString();
+
+// Answer: Convert.ToString(age);
+
+
+// 5. How do you add a single-line comment in C#?
+
+// ' Comment '
+// " Comment "
+// ## Comment
+// /* Comment */
+// // Comment
+
+// Answer: // Comment
+
+
+// 6. Which data type would you use to store a large whole number?
+
+// bool
+// float
+// long
+// char
+// string
+
+// Answer: long
+
+
+// 7. What are the three components of a for loop?
+
+// Variable, increment and condition
+// Start, end and step
+// Counter, increment and condition
+// Initialization, increment and body
+// Initialization, condition and final expression
+
+// Answer: Variable, increment and condition
+
+
+// 8. How do you resume program execution after hitting a breakpoint in VS Code?
+
+// Proceed
+// Run
+// Continue
+// Restart
+// Play
+
+// Answer: Continue
+
+
+// 9. What is the syntax to return a value from a method?
+
+// return value;
+// return value();
+// result = value;
+// yield return value;
+// return(value);
+
+// Answer: return value;
+
+
+// 10. What is the output of the following code snippet?
+
+// string name = "Alice";
+// int age = 25;
+// Console.WriteLine($"My name is {name} and I'm {age} years old.");
+
+// The code will result in an error.
+// My name is Alice and I'm 25 years.
+// My name is Alice and I'm 25 years old.
+// My name is Alice, and I'm 25 years old.
+// My name is {name} and I'm {age} years old.
+
+// Answer: My name is Alice and I'm 25 years old.
